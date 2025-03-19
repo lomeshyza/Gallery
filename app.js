a1bebc22ffd0221ddde69475771c4a73ac6be974
Nadya Gallyamova <lomeshyza@yandex.ru>
2025-03-19 21:53:30 +0300

    feat: Программное добавление активного класса

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
-const slides=document.querySelectorAll('.slide')
+function slidesPlugin (activeSlide=4){
+    const slides=document.querySelectorAll('.slide')
 
-for(const slide of slides) {
-    slide.addEventListener('click', ()=>{
-        clearActiveClasses()
+    slides[activeSlide].classList.add('active')
 
-        slide.classList.add('active')
-
-    })
+    for(const slide of slides) {
+        slide.addEventListener('click', ()=>{
+            clearActiveClasses()
+            slide.classList.add('active')
+        })
+    }
+    function clearActiveClasses(){
+        slides.forEach((slide)=>{
+            slide.classList.remove('active')
+        })
+    }
 }
-function clearActiveClasses(){
-    slides.forEach((slide)=>{
-        slide.classList.remove('active')
-    })
-}
+slidesPlugin(4)
